f258f6b7a79b9a861e7871c7f9835c949c820e86
Norbert Kowalik <norbert.kowalik@icloud.com>
2022-11-22 17:22:55 +0100

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,41 @@
+<script setup>
+import data from './data/sim/2022.data'
+
+</script>
+
+<template>
+  <main>
+      <table>
+          <thead>
+            <tr>
+                <th v-for="(item, index) in data[0]">
+                    {{ item.driver }}
+                </th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr v-for="entry in data">
+                <td v-for="({points, maxPoints, position, maxPosition, minPosition}, index) in entry">
+                    {{ points }} - {{ position }} ({{ maxPosition }} - {{ minPosition }})
+                </td>
+            </tr>
+          </tbody>
+      </table>
+  </main>
+</template>
+
+<style scoped>
+table, td, th {
+    border: 1px solid black;
+    border-collapse: collapse;
+}
+
+td, th {
+    padding: 0 2px;
+}
+
+th {
+    font-weight: bold;
+}
+
+</style>
---
+++ src/dataBuild/data-build-plugin.js
@@ -0,0 +1,157 @@
+const fileRegex = /([0-9]{4})\.data$/
+
+export default {
+    name: 'data-build',
+    transform(src, id) {
+        if (!fileRegex.test(id)) {
+            return;
+        }
+        const data = JSON.parse(src);
+        const year = fileRegex.exec(id)[1]
+        const converted = convert(data, year)
+
+        return 'export default ' + JSON.stringify(converted);
+    }
+};
+
+function convert(input, year) {
+    const { races, pointSchema, remainingRaces } = input;
+    const drivers = Object.keys(input.drivers)
+
+    const result = [];
+
+    for (let i = 0; i <= races.length; i++) {
+        const racesPart = races.slice(0, i);
+        const remainingRacesInSeason = races.slice(i).concat(remainingRaces)
+        const maxPoints = calculateMaxAvailablePoints(remainingRacesInSeason, pointSchema)
+        result.push(calculateResults(
+            year,
+            drivers,
+            pointSchema,
+            racesPart === undefined ? [] : racesPart,
+            remainingRacesInSeason.length,
+            maxPoints,
+        ));
+    }
+
+    return result;
+}
+
+function calculateMaxAvailablePoints(remainingRacesInSeason, pointSchemas) {
+    let maxPoints = 0
+    for (const { type } of remainingRacesInSeason) {
+        const { points, fastestLap } = pointSchemas[type];
+        maxPoints += points[0];
+        if (fastestLap !== undefined) {
+            maxPoints += fastestLap.value;
+        }
+    }
+
+    return maxPoints;
+}
+
+function calculateResults(year, drivers, pointSchemas, races, remainingWins, maxRemainingPoints) {
+    const dataResult = calculateResultAfterRaces(year, drivers, pointSchemas, races);
+
+    let result = [];
+
+    Object.keys(dataResult).forEach((driver) => {
+        result.push({
+            driver,
+            points: dataResult[driver].points,
+            maxPoints: dataResult[driver].points + maxRemainingPoints,
+            racePositions: dataResult[driver].positionsCount,
+            position: 0,
+            maxPosition: 0,
+            minPosition: 0,
+        })
+    })
+
+    const sortedByPosition = sortByPositions(result, r => r.points);
+
+    result = result.map(
+        (entry) => {
+            const driver = entry.driver;
+            const sortedForBestCase = sortByPositions(result, r => r.driver === driver ? r.maxPoints : r.points);
+            const sortedForWorstCase = sortByPositions(result, r => r.driver === driver ? r.points : r.maxPoints);
+
+            entry.position = sortedByPosition.indexOf(driver) + 1;
+            entry.maxPosition = sortedForBestCase.indexOf(driver) + 1;
+            entry.minPosition = sortedForWorstCase.indexOf(driver) + 1;
+
+            return entry;
+        }
+    );
+
+    return result;
+}
+
+function sortByPositions(results, pointsSupplier) {
+    return [...results]
+        .sort(buildComparator(pointsSupplier))
+        .map(entry => entry.driver)
+}
+
+function buildComparator(pointsSupplier) {
+    return (a, b) => {
+        const aPoints = pointsSupplier(a);
+        const bPoints = pointsSupplier(b);
+        if (aPoints !== bPoints) {
+            return bPoints - aPoints;
+        }
+
+        for (let i = 1; i < 50; i++) {
+            const aPosition = a.racePositions[i] ?? 0;
+            const bPosition = b.racePositions[i] ?? 0;
+            if (aPosition !== bPosition) {
+                return bPosition - aPosition;
+            }
+        }
+
+        return 0;
+    }
+}
+
+function calculateResultAfterRaces(year, drivers, pointSchemas, races) {
+    const result = prepareEmptyResults(drivers);
+
+    for (const { type, fastestLap, positions, code } of races) {
+        const pointSchema = pointSchemas[type];
+
+        for (let i = 0; i < positions.length; i++) {
+            const driver = positions[i];
+            const position = i + 1;
+
+            if (!result.hasOwnProperty(driver)) {
+                throw new Error(`Non-existing driver ${driver} in race code ${code} [${type}], year ${year}`)
+            }
+
+            result[driver].points += pointSchema.points[position - 1] ?? 0;
+
+            if (pointSchema.positionsCount) {
+                if (!result[driver].positionsCount.hasOwnProperty(position)) {
+                    result[driver].positionsCount[position] = 0;
+                }
+                result[driver].positionsCount[position]++;
+            }
+
+            if (position <= pointSchema.fastestLap.maxPosition && driver === fastestLap) {
+                result[driver].points += pointSchema.fastestLap.value;
+            }
+        }
+    }
+
+    return result;
+}
+
+function prepareEmptyResults(drivers) {
+    const result = {};
+    for (const driver of drivers) {
+        result[driver] = {
+            points: 0,
+            positionsCount: {}
+        }
+    }
+
+    return result;
+}
