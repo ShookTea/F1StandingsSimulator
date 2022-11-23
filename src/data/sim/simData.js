ff43a50b09d54eee91cd1682d89f0b3486377ae7
Norbert Kowalik <norbert.kowalik@icloud.com>
2022-11-23 18:43:38 +0100

    Add data for 2021

--- src/data/sim/simData.js
+++ src/data/sim/simData.js
@@ -1,8 +1,10 @@
 import data2022 from './2022.data';
+import data2021 from './2021.data';
 
 const allYears = {
     2022: data2022,
+    2021: data2021,
 }
 
 export default allYears;
-export const newestYear = Object.keys(allYears)[0];
+export const newestYear = Object.keys(allYears).sort().pop();
