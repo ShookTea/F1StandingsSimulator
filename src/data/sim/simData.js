3d83a5ae6a83de6fb3967a9f5a5e854448873e84
Norbert Kowalik <norbert.kowalik@icloud.com>
2022-11-23 19:12:31 +0100

    Added entry for season 2024

--- src/data/sim/simData.js
+++ src/data/sim/simData.js
@@ -1,9 +1,11 @@
-import data2022 from './2022.data';
 import data2021 from './2021.data';
+import data2022 from './2022.data';
+import data2023 from './2023.data';
 
 const allYears = {
-    2022: data2022,
     2021: data2021,
+    2022: data2022,
+    2024: data2023,
 }
 
 export default allYears;
