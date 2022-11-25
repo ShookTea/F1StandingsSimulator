c31091764dd73f225005dcc81e516c1919e7c335
Norbert Kowalik <norbert.kowalik@icloud.com>
2022-11-25 15:27:21 +0100

    Added comparison methods

--- src/dataBuild/RacePositionMapping.ts
+++ src/dataBuild/RacePositionMapping.ts
@@ -12,4 +12,29 @@ export default class RacePositionMapping {
             this.racePositions[position]++;
         }
     }
+
+    private getLowestPosition(): number
+    {
+        return Object.keys(this.racePositions).map(parseInt).sort().reverse()[0] ?? 0;
+    }
+
+    private getOccurencesInPosition(position: number): number
+    {
+        return this.racePositions[position] ?? 0;
+    }
+
+    compareWith(other: RacePositionMapping): number
+    {
+        const searchToPosition = Math.max(this.getLowestPosition(), other.getLowestPosition());
+
+        for (let position = 1; position <= searchToPosition; position++) {
+            const currentValue = this.getOccurencesInPosition(position);
+            const otherValue = other.getOccurencesInPosition(position);
+            if (currentValue !== otherValue) {
+                return otherValue - currentValue;
+            }
+        }
+
+        return 0;
+    }
 }
