43939377b988e015065ead0770178d93f9ae9999
Norbert Kowalik <norbert.kowalik@icloud.com>
2022-11-26 18:39:48 +0100

    Getting correct max pages

--- src/components/SimulationView.vue
+++ src/components/SimulationView.vue
@@ -1,22 +1,23 @@
 <template>
     <div class="simulation-view">
         <h3>{{ { meta: $route.meta, params: $route.params } }}</h3>
-        <simulation-pager label="foo" route-key="step" :max-pages="10"/>
+        <simulation-pager label="foo" route-key="step" :max-pages="seasonData.data.length"/>
         <simulation-season-view :season-data="seasonData"/>
     </div>
 </template>
 
 <script lang="ts">
-import allData from '@/data/sim/data';
+import allData, { SeasonData } from '@/data/sim/data';
 import SimulationSeasonView from '@/components/SimulationSeasonView.vue';
 import SimulationPager from '@/components/SimulationPager.vue';
+import { Season } from '@/data/sim/f1/simDataTypes';
 
 export default {
     components: { SimulationPager, SimulationSeasonView },
     computed: {
-        seasonData() {
+        seasonData(): SeasonData<Season> {
             const sportData = allData.find(entry => entry.routePart === this.$route.meta.sport);
-            return sportData.data.find(entry => entry.routePart === this.$route.meta.season);
+            return <SeasonData<Season>><unknown>sportData.data.find(entry => entry.routePart === this.$route.meta.season);
         }
     }
 }
