297ba78e5f785f12f7a70ea48799b531ff5cbedd
Norbert Kowalik <norbert.kowalik@icloud.com>
2022-11-26 16:42:45 +0100

    Basics of control panel

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,23 @@
 <script setup>
-import SimulationView from '@/components/SimulationView.vue';
+import AppHeader from '@/components/header/AppHeader.vue';
 </script>
 
 <template>
   <main>
-      <header>
-          <h1>F1 season simulator</h1>
-      </header>
+      <app-header/>
       <router-view/>
   </main>
 </template>
 
 <style>
-body {
+html, body, main {
     line-height: 1.6;
     font-family: Inter, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu,
                  Cantarell, 'Fira Sans', 'Droid Sans', 'Helvetica Neue', sans-serif;
     font-size: 15px;
     background-color: white;
+    padding: 0;
+    margin: 0;
     color: black;
 }
 </style>
---
+++ src/components/header/AppHeader.vue
@@ -0,0 +1,47 @@
+<template>
+    <header>
+        <h1>F1 Season Simulator</h1>
+        <div class="header-control-block">
+            <div @click="selected = 0" class="header-control-block-entry" :class="selected === 0 ? 'selected' : null">
+                Formula 1
+            </div>
+            <div @click="selected = 1" class="header-control-block-entry" :class="selected === 1 ? 'selected' : null">
+                Formula 2
+            </div>
+        </div>
+    </header>
+</template>
+
+<script lang="ts">
+export default {
+    data() {
+        return {
+            selected: 0,
+        }
+    }
+}
+</script>
+
+<style scoped>
+    header {
+        background-color: darkblue;
+        padding-left: 1em;
+    }
+    h1 {
+        margin-top: 0;
+        margin-bottom: 0;
+        color: white;
+    }
+    .header-control-block {
+        color: white;
+        padding-bottom: 1em;
+        display: flex;
+        flex-direction: row;
+    }
+    .header-control-block-entry {
+        margin-right: 1em;
+    }
+    .selected {
+        font-weight: bold;
+    }
+</style>
