f88533749593f298d5e693a67e47c5a45007c7c1
Norbert Kowalik <norbert.kowalik@icloud.com>
2022-11-30 21:17:16 +0100

    Displaying github button

--- src/components/header/AppHeader.vue
+++ src/components/header/AppHeader.vue
@@ -4,12 +4,12 @@
             <h1>F1 Season Simulator</h1>
             <div class="button-panel">
                 <div class="button sponsor-button" @click="sponsorButton">
-                    <svg height="16" width="16">
-                        <path fill="red" d="M4.25 2.5c-1.336 0-2.75 1.164-2.75 3 0 2.15 1.58 4.144 3.365 5.682A20.565 20.565 0 008 13.393a20.561 20.561 0 003.135-2.211C12.92 9.644 14.5 7.65 14.5 5.5c0-1.836-1.414-3-2.75-3-1.373 0-2.609.986-3.029 2.456a.75.75 0 01-1.442 0C6.859 3.486 5.623 2.5 4.25 2.5zM8 14.25l-.345.666-.002-.001-.006-.003-.018-.01a7.643 7.643 0 01-.31-.17 22.075 22.075 0 01-3.434-2.414C2.045 10.731 0 8.35 0 5.5 0 2.836 2.086 1 4.25 1 5.797 1 7.153 1.802 8 3.02 8.847 1.802 10.203 1 11.75 1 13.914 1 16 2.836 16 5.5c0 2.85-2.045 5.231-3.885 6.818a22.08 22.08 0 01-3.744 2.584l-.018.01-.006.003h-.002L8 14.25zm0 0l.345.666a.752.752 0 01-.69 0L8 14.25z"></path>
-                    </svg>
-                    <span>
-                        Donate
-                    </span>
+                    <v-icon class="icon" name="bi-heart-fill"/>
+                    <span>Donate</span>
+                </div>
+                <div class="button github-button" @click="githubButton">
+                    <v-icon class="icon" name="bi-github"/>
+                    <span>GitHub</span>
                 </div>
             </div>
         </div>
@@ -38,6 +38,9 @@ export default {
     methods: {
         sponsorButton(): void {
             window.location.href = 'https://github.com/sponsors/ShookTea';
+        },
+        githubButton(): void {
+            window.location.href = 'https://github.com/ShookTea/F1StandingsSimulator';
         }
     },
     watch: {
@@ -71,6 +74,9 @@ export default {
     }
     .button-panel {
         margin-right: 2em;
+        display: flex;
+        flex-direction: row;
+        gap: 1em;
     }
     .button {
         border-radius: 2em;
@@ -84,5 +90,12 @@ export default {
     }
     .sponsor-button {
         background-color: lightblue;
+    }
+    .sponsor-button .icon {
+        color: deeppink;
+    }
+    .github-button {
+        background-color: #303030;
+        color: white;
     }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,13 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from '@/routing';
+import { OhVueIcon, addIcons } from "oh-vue-icons";
+import { BiGithub, BiHeartFill } from "oh-vue-icons/icons";
+
+addIcons(BiGithub, BiHeartFill);
 
 const app = createApp(App);
+app.component("v-icon", OhVueIcon);
 app.use(router);
 app.mount('#app');
 
