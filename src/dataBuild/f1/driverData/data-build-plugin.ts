75512840944afe08e8cd7675bf23c06f8851514a
Norbert Kowalik <norbert.kowalik@icloud.com>
2022-12-15 16:54:44 +0100

    Preparing list of files to read

--- src/dataBuild/f1/driverData/data-build-plugin.ts
+++ src/dataBuild/f1/driverData/data-build-plugin.ts
@@ -1,4 +1,17 @@
+// @ts-ignore
+import fs from 'node:fs';
+import drivers from '../../../data/racing_drivers.json';
+
 const fileRegex: RegExp = /sim\/formulaDrivers\/driver\.data$/;
+
+type SimDataDirectory = { sport: string, path: string };
+type SimDataSource = { sport: string, season: string, path: string };
+
+const simDataDirectories: SimDataDirectory[] = [
+    { sport: 'Formula 1', path:'src/data/sim/f1' },
+];
+
+const simDataRegex: RegExp = /^([0-9]{4})\.data$/
 
 export default {
     name: 'data-build',
@@ -9,6 +22,22 @@ export default {
         // const data: DataInput = JSON.parse(src);
         // const converted: Season = convert(data)
 
+        // fs.readFileSync('../../../data/sim/f1/2019.data');
+        const files: SimDataSource[] = simDataDirectories
+          .flatMap(dir => listSourceFilesFromDirectory(dir))
+        ;
         return 'export default ' + JSON.stringify({foo: 'bar'});
     }
 };
+
+function listSourceFilesFromDirectory(directory: SimDataDirectory): SimDataSource[]
+{
+    return fs.readdirSync(directory.path)
+      .filter(file => simDataRegex.test(file))
+      .map(file => simDataRegex.exec(file))
+      .map(regexResult => ({
+          sport: directory.sport,
+          season: regexResult[1],
+          path: `${directory.path}/${regexResult[0]}`
+      }));
+}
