490831fbcb420b339cd6fd764d1e07ae6f654a6b
Norbert Kowalik <norbert.kowalik@icloud.com>
2022-12-30 17:31:32 +0100

    Loading team strategy for driver

--- src/dataBuild/f1/model/Driver.ts
+++ src/dataBuild/f1/model/Driver.ts
@@ -1,16 +1,20 @@
 import { DataInput, DriverEntry } from '@/dataBuild/f1/dataInputTypes';
+import AbstractDriverTeamStrategy from './teamStrategy/AbstractDriverTeamStrategy';
+import TeamStrategyBuilder from './teamStrategy/TeamStrategyBuilder';
 
 export default class Driver {
   readonly driverAbbr: string;
   readonly uuid: string;
   readonly temporary: boolean;
   readonly number: number|null;
+  readonly team: AbstractDriverTeamStrategy;
 
   constructor(driverAbbr: string, dataInput: DriverEntry) {
     this.driverAbbr = driverAbbr;
     this.uuid = dataInput.uuid;
     this.temporary = dataInput.temporary ?? false;
     this.number = dataInput.number;
+    this.team = TeamStrategyBuilder.buildStrategy(this, dataInput);
   }
 
   static buildAllFromInput(dataInput: DataInput): Driver[]
