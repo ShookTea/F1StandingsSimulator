12bd9d5027b43de76f8f2ab647f14cb0087206e0
Norbert Kowalik <norbert.kowalik@icloud.com>
2022-12-31 07:44:05 +0100

    Loading fastest lap driver

--- src/dataBuild/f1/model/Race.ts
+++ src/dataBuild/f1/model/Race.ts
@@ -1,9 +1,13 @@
 import AbstractRace from './AbstractRace';
 import { Race as RaceModel } from '../dataInputTypes';
 import Season from './Season';
+import Driver from './Driver';
 
 export default class Race extends AbstractRace {
+  private readonly fastestLapDriver: Driver|null;
+
   constructor(roundNumber: number, raceModel: RaceModel, season: Season) {
     super(roundNumber, raceModel, season, raceModel.teamSwitch);
+    this.fastestLapDriver = raceModel.fastestLap === null ? null : season.getDriverByAbbreviation(raceModel.fastestLap);
   }
 }
