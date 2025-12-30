32f38da6b3fa3d0624eb5c870bc8dcebe6de1a4d
Norbert Kowalik <norbert.kowalik@icloud.com>
2025-12-30 11:16:19 +0100

    Experiments to confirm results

--- src/dataBuild/f1/seasonData/head-to-head.ts
+++ src/dataBuild/f1/seasonData/head-to-head.ts
@@ -1,7 +1,11 @@
-import { Standing, Driver } from '@/data/sim/f1/simDataTypes';
+import { Standing, Driver, StandingOwner } from '@/data/sim/f1/simDataTypes';
 import { AbstractRace, DataInput, Race } from '../dataInputTypes';
 import DriverStanding from '../DriverStanding';
 import * as lodash from 'lodash';
+import AbstractStanding from '../AbstractStanding';
+import { SorterBuilder } from '../AbstractStandingSorter';
+import AbstractStandingResultStore from '../AbstractStandingResultStore';
+import DriverStandingSorter from '../DriverStandingSorter';
 
 type HeadToHeadSituation = {
   standings: Standing<Driver>[];
@@ -27,6 +31,7 @@ export function generateHeadToHeads(
     input,
     situation,
     remainingRaces[0],
+    remainingRaces,
     baseStandings,
   ));
 }
@@ -58,11 +63,13 @@ function simulateHeadToHeadRace(
   input: DataInput,
   situation: HeadToHeadSituation,
   nextRace: AbstractRace,
+  remainingRaces: AbstractRace[],
   baseStandings: DriverStanding[],
 ) {
-  const pointSchema = input.pointSchemas[nextRace.typeBeforeRace ?? nextRace.type];
   const positions = [...new Array(baseStandings.length).keys()];
   const positionPermutations = kPermutations(positions, situation.standings.length);
+  const baseStandingsToTest: DriverStanding[] = 
+    situation.standings.map((standing) => baseStandings.find((s) => s.owner.abbreviation === standing.owner.abbreviation));
 
   for (const permutation of positionPermutations) {
     const testPositions: string[] = new Array(baseStandings.length).fill('???');
@@ -74,9 +81,26 @@ function simulateHeadToHeadRace(
 
     const fakeRace: Race = {
       ...nextRace,
-      fastestLap: undefined,
+      fastestLap: undefined, // TODO: run simulations with different fastest laps
       positions: testPositions,
     };
+
+    const standingsForExperiment = lodash.cloneDeep(baseStandingsToTest);
+    standingsForExperiment.forEach((standing) => standing.addRaceResult(input, fakeRace));
+    
+    const result = calculateResultsForStandings(standingsForExperiment, remainingRaces.slice(1), input, DriverStandingSorter.getBuilder());
+    if (situation.position === 1 && situation.standings.length === 3 && nextRace.code === 'ABU'
+      && permutation[0] + 1 === 6 // NOR
+      && permutation[1] + 1 === 2 // VER
+      && permutation[2] + 1 === 1 // PIA
+    ) {
+      console.log(`Situation: ${situation.standings.map(s => s.owner.abbreviation).join(' ')}`)
+      console.log(`Permutation: ${permutation.map(v => v + 1).join(' ')}`);
+      for (const testedStanding of situation.standings) {
+        const driverResult = result.find(r => r.owner.abbreviation === testedStanding.owner.abbreviation);
+        console.log(`${driverResult.owner.abbreviation}: ${driverResult.points} won, position between ${driverResult.minPosition} - ${driverResult.maxPosition}`);
+      }
+    }
   }
 }
 
@@ -105,4 +129,23 @@ function kPermutations<T>(arr: T[], k: number): T[][] {
   }
   backtrack();
   return result;
+}
+
+function calculateResultsForStandings<S extends StandingOwner>(
+  standingResult: AbstractStanding<S>[],
+  remainingRaces: AbstractRace[],
+  input: DataInput,
+  sorterBuilder: SorterBuilder<AbstractStandingResultStore<S>>
+): Standing<S>[] {
+  const dataResult = standingResult.map(s => s.toStandingResult(remainingRaces, input));
+  const comparator = sorterBuilder.buildSorter();
+
+  comparator.resetSorter(dataResult);
+  dataResult
+      .sort((a, b) => comparator.compare(a, b))
+      .forEach((standing, index) => standing.position = index + 1);
+
+  dataResult.forEach(d => d.calculatePossiblePositions(dataResult, sorterBuilder));
+
+  return dataResult.map(s => s.convertToResultObject());
 }
