3bf5cd037749fba4853a7bacf3fbf82ce13b2009
Norbert Kowalik <norbert.kowalik@icloud.com>
2025-12-30 15:50:49 +0100

    Refactor

--- src/components/formula/FormulaPageView.vue
+++ src/components/formula/FormulaPageView.vue
@@ -1,3 +1,25 @@
+<script lang="ts" setup>
+import allData, { GroupData } from '@/data/sim/data';
+import { Round, Season } from '@/data/sim/f1/simDataTypes';
+import FormulaRoundPager from '@/components/formula/FormulaRoundPager.vue';
+import FormulaRoundTable from '@/components/formula/FormulaRoundTable.vue';
+import { useRoute } from 'vue-router';
+import { computed } from 'vue';
+
+const route = useRoute();
+
+const seasonData = computed<GroupData<Season>>(() => {
+    const sportData = allData.find(entry => entry.routePart === route.meta.sport);
+    return <GroupData<Season>><unknown>sportData.data.find(entry => entry.routePart === route.meta.season);
+});
+
+const currentPage = computed<Round>(() => {
+    const step: number = parseInt(route.params.step as string) - 1;
+    console.log({ step });
+    return seasonData.value.data[step];
+});
+</script>
+
 <template>
     <div class="simulation-view">
         <formula-round-pager :label="currentPage.roundName" route-key="step" :max-pages="seasonData.data.length"/>
@@ -5,27 +27,6 @@
     </div>
 </template>
 
-<script lang="ts">
-import allData, { GroupData } from '@/data/sim/data';
-import { Round, Season } from '@/data/sim/f1/simDataTypes';
-import FormulaRoundPager from '@/components/formula/FormulaRoundPager.vue';
-import FormulaRoundTable from '@/components/formula/FormulaRoundTable.vue';
-
-export default {
-    components: { FormulaRoundTable, FormulaRoundPager },
-    computed: {
-        seasonData(): GroupData<Season> {
-            const sportData = allData.find(entry => entry.routePart === this.$route.meta.sport);
-            return <GroupData<Season>><unknown>sportData.data.find(entry => entry.routePart === this.$route.meta.season);
-        },
-        currentPage(): Round {
-            const step: number = parseInt(this.$route.params.step) - 1;
-
-            return this.seasonData.data[step];
-        },
-    }
-}
-</script>
 <style scoped>
 .simulation-view {
     display: flex;
