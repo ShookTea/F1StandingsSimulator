146eb720b47163d23f869e8d220c6aff6c07762e
Norbert Kowalik <norbert.kowalik@icloud.com>
2025-12-30 15:08:44 +0100

    Better styling of head-to-head situations

--- src/components/formula/FormulaTableDriverRow.vue
+++ src/components/formula/FormulaTableDriverRow.vue
@@ -24,28 +24,41 @@ const fullName = computed<string>(() => {
     return `${givenName} ${familyName}`;
 });
 
+const getHeadToHeadIndex = (headToHead: HeadToHead) => {
+    const minPosition = headToHead.leadPosition;
+    const totalDriversInSituation = headToHead.drivers.length;
+    const maxPosition = headToHead.leadPosition + totalDriversInSituation - 1;
+    return { minPosition, maxPosition };
+}
+
 const headToHead = computed<HeadToHead | null>(() => {
     for (const entry of props.headToHeads) {
-        const minPosition = entry.leadPosition;
-        const totalDriversInSituation = Math.max(
-            ...Object.values(entry.optionsByDriver)
-                .flatMap((e) => e)
-                .flatMap((e) => Object.keys(e.rivals).length)
-            );
-        
-        const maxPosition = entry.leadPosition + totalDriversInSituation;
+        const { minPosition, maxPosition } = getHeadToHeadIndex(entry);
         const currentPosition = props.index + 1;
-
         if (currentPosition >= minPosition && currentPosition <= maxPosition) {
             return entry;
         }
     }
     return null;
 });
+
+const headToHeadFirst = computed<boolean>(() => headToHead.value && headToHead.value.leadPosition === props.index + 1);
+const headToHeadLast = computed<boolean>(() => {
+    if (!headToHead.value) {
+        return false;
+    }
+    const { maxPosition } = getHeadToHeadIndex(headToHead.value);
+    return maxPosition === props.index + 1;
+})
 </script>
 
 <template>
-    <tr :class="[headToHead ? 'head-to-head' : null]">
+    <tr :class="[
+        headToHead ? 'head-to-head' : null,
+        headToHeadFirst ? 'head-to-head-first' : null,
+        headToHeadLast ? 'head-to-head-last' : null,
+        ]"
+    >
         <th class="pre-cell">{{ standing.owner.number }}</th>
         <th class="pre-cell">
             <div class="driver-abbr">
@@ -74,6 +87,12 @@ const headToHead = computed<HeadToHead | null>(() => {
 <style>
 tr.head-to-head {
     background-color: cyan;
+}
+tr.head-to-head.head-to-head-first {
+    background: linear-gradient(to bottom, transparent, cyan, cyan);
+}
+tr.head-to-head.head-to-head-last {
+    background: linear-gradient(to top, transparent, cyan, cyan);
 }
 th {
     border: 1px solid #606060;
