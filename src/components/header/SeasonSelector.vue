936715b995bb432091e4f022ebc27358b8d97096
Norbert Kowalik <norbert.kowalik@icloud.com>
2025-12-30 10:17:37 +0100

    Refactor of SeasonSelector component

--- src/components/header/SeasonSelector.vue
+++ src/components/header/SeasonSelector.vue
@@ -1,14 +1,58 @@
 <script lang="ts" setup>
+import { GroupData, SportData } from '@/data/sim/data';
+import { ref, computed, watch } from 'vue';
+import { useRoute, useRouter } from 'vue-router';
+
 interface Props {
     sportData: SportData<any>
     enabled: boolean
 }
-
-defineProps<Props>();
-defineEmits<{
+const props = defineProps<Props>();
+const emit = defineEmits<{
     (e: 'enable'): void
 }>();
+
+const selectedOption = ref<number>(0);
+const route = useRoute();
+const router = useRouter();
+
+
+const availableOptions = computed<string[]>(
+    () => props.sportData.data.map((entry) => entry.label),
+);
+
+const goToIndex = (index: number) => {
+    const selectedEntry: GroupData<any> = props.sportData.data[index];
+    const path: string = `/${props.sportData.routePart}/${selectedEntry.routePart}`;
+    router.push(path);
+}
+
+const switchToMode = () => {
+    selectedOption.value = 0;
+    emit('enable');
+    goToIndex(0);
+}
+
+watch(
+    selectedOption,
+    (newValue: number) => {
+        goToIndex(newValue);
+    }
+);
+
+watch(
+    route,
+    () => {
+        if (!props.enabled) return;
+        const routeKeys = props.sportData.data.map(entry => entry.routePart);
+        const index = routeKeys.findIndex(k => k === route.meta.season);
+        if (index !== -1) {
+            selectedOption.value = index;
+        }
+    }
+);
 </script>
+
 <template>
     <div class="season-selector-container">
         <div class="mode-switch" v-if="!enabled" @click="() => switchToMode()">{{ sportData.label }}</div>
@@ -20,50 +64,6 @@ defineEmits<{
         </div>
     </div>
 </template>
-
-<script lang="ts">
-
-import { SportData } from '@/data/sim/data';
-
-export default {
-    data() {
-        return {
-            selectedOption: 0,
-        }
-    },
-    computed: {
-        availableOptions(): string[] {
-            return this.sportData.data.map(entry => entry.label);
-        },
-    },
-    watch: {
-        selectedOption(newValue: number) {
-            this.goToIndex(newValue);
-        },
-        $route() {
-            if (!this.enabled) return;
-            const routeKeys = this.sportData.data.map(entry => entry.routePart);
-            const index = routeKeys.findIndex(k => k === this.$route.meta.season);
-
-            if (index !== -1) {
-                this.selectedOption = index;
-            }
-        }
-    },
-    methods: {
-        switchToMode(): void {
-            this.selectedOption = 0;
-            this.$emit('enable');
-            this.goToIndex(0);
-        },
-        goToIndex(index: number): void {
-            const selectedEntry: SportData<any> = this.sportData.data[index];
-            const path: String = `/${this.sportData.routePart}/${selectedEntry.routePart}`;
-            this.$router.push(path);
-        },
-    }
-}
-</script>
 
 <style scoped>
 .season-selector-container {
